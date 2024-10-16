97ae4e3ad2c4b0d2bcd1739cc886c42a958119b4
Milind Mishra <milind.mishra4@gmail.com>
2024-10-17 00:04:25 +0530

    feat: update profile and add links to the recent commits table

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -11,6 +11,13 @@ import {
 import { env } from "@/env";
 import { Suspense } from "react";
 import Loader from "@/components/loader";
+import GitHubCalendar from "react-github-calendar";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Link from "next/link";
+import { Link2 } from "lucide-react";
+
+dayjs.extend(relativeTime);
 
 async function getUserData(username: string) {
   const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/api/user/${username}`, {
@@ -43,6 +50,7 @@ export default async function UserPage({
             </div>
           </CardHeader>
           <CardContent>
+            <GitHubCalendar username={user?.login} />
             <h2 className="text-xl font-semibold mb-4">Recent Commits</h2>
             <Table>
               <TableHeader>
@@ -56,11 +64,22 @@ export default async function UserPage({
                 {/* @ts-expect-error : event type is not defined */}
                 {events?.map((event) => (
                   <TableRow key={event?.id}>
-                    <TableCell>{event?.repo?.name}</TableCell>
-                    <TableCell>{event?.payload?.commits[0]?.message}</TableCell>
                     <TableCell>
-                      {new Date(event?.created_at).toLocaleDateString()}
+                      <Link
+                        href={event?.repo?.url?.replace(
+                          "https://api.github.com/repos/",
+                          "https://github.com/"
+                        )}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center"
+                      >
+                        {event?.repo?.name}
+                        <Link2 className="h-3 w-3 ml-1" />
+                      </Link>
                     </TableCell>
+                    <TableCell>{event?.payload?.commits[0]?.message}</TableCell>
+                    <TableCell>{dayjs(event?.created_at).fromNow()}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
